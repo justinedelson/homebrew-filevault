f36c0557333158ca06b1fa8319261b3a1131b6d4
Justin Edelson <justin@justinedelson.com>
2013-10-18 08:26:01 -0400

    updating to jr release 3.0.0

--- filevault.rb
+++ filevault.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Filevault < Formula
   homepage 'http://dev.day.com/docs/en/crx/current/how_to/how_to_use_the_vlttool.html'
-  url 'https://repository.apache.org/content/repositories/snapshots/org/apache/jackrabbit/vault/vault-cli/3.0.0-SNAPSHOT/vault-cli-3.0.0-20130816.135705-1-bin.tar.gz'
-  sha1 'cecdf3b025ba016b7ed9a0f50a8921ebb64367fb'
-  version '3.0.0-SNAPSHOT'
+  url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.0.0/vault-cli-3.0.0-bin.tar.gz'
+  sha1 'f92b9bf44495bff37fec1c6311e3a28758d23c58'
+  version '3.0.0'
 
   def install
     # Remove windows files
@@ -16,9 +16,6 @@ class Filevault < Formula
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test vault`.
     system "vlt"
   end
 end
