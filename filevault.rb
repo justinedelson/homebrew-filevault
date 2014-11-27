fd23c1b7f12fd5b3454414c1b78b41a09dbcf8c8
justinedelson <justin@justinedelson.com>
2014-11-27 11:47:46 -0500

    updating to 3.1.8

--- filevault.rb
+++ filevault.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Filevault < Formula
   homepage 'http://dev.day.com/docs/en/crx/current/how_to/how_to_use_the_vlttool.html'
-  url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.0.0/vault-cli-3.0.0-bin.tar.gz'
-  sha1 'f92b9bf44495bff37fec1c6311e3a28758d23c58'
-  version '3.0.0'
+  url 'http://repo.maven.apache.org/maven2/org/apache/jackrabbit/vault/vault-cli/3.1.8/vault-cli-3.1.8-bin.tar.gz'
+  sha1 '7d25e5016963ec109be7234e28ee2815608b1da1'
+  version '3.1.8'
 
   def install
     # Remove windows files
